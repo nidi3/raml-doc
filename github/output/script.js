4bc3d1f1f778a60d8e34e849f4226955645a79b4
Stefan Niederhauser <ghuder5@gmx.ch>
2015-08-17 01:19:06 +0200

    generate with 0.0.4-SNAPSHOT

--- github/output/script.js
+++ github/output/script.js
@@ -13,6 +13,12 @@ var rd = (function () {
         return elem;
     }
 
+    function nextSiblingWithClass(elem, clazz) {
+        return findNextSibling(elem, function (e) {
+            return hasClass(e, clazz);
+        });
+    }
+
     function doWithChildren(elem, action) {
         var e = elem.firstChild;
         while (e) {
@@ -33,7 +39,49 @@ var rd = (function () {
         return node.className && node.className.indexOf(clazz) >= 0;
     }
 
+    function wrap(node, wrapper) {
+        var next = node.nextSibling, parent = node.parentNode;
+        parent.removeChild(node);
+        wrapper.appendChild(node);
+        if (next) {
+            parent.insertBefore(wrapper, next);
+        } else {
+            parent.appendChild(wrapper);
+        }
+    }
+
+    function parseQuery(q) {
+        var i, parts, res = {}, params = q.substring(1).split('&');
+        if (q.charAt(0) === '?') {
+            for (i = 0; i < params.length; i++) {
+                parts = params[i].split('=');
+                res[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+            }
+        }
+        return res;
+    }
+
     return {
+        setBaseUri: function (uri) {
+            var href = window.location.href,
+                hostPos = href.indexOf('://'),
+                pathPos = href.indexOf('/', hostPos + 3),
+                endPathPos = href.indexOf('/resource/'),
+                host = href.substring(hostPos + 3, pathPos),
+                path = href.substring(pathPos + 1, endPathPos);
+            rd.baseUri = normalize(uri.replace('$host', host).replace('$path', path + '/../..'));
+
+            function normalize(path) {
+                var len;
+                path = path.replace(/\/\.\//g, '/');
+                do {
+                    len = path.length;
+                    path = path.replace(/\/[^\/]+\/\.\.\//, '/');
+                } while (path.length != len);
+                return path;
+            }
+        },
+
         showTrWithId: function (elem, id) {
             var show = null;
             doWithChildren(findParent(elem, 'tbody'), function (tr) {
@@ -53,20 +101,20 @@ var rd = (function () {
             });
         },
         showActionDetail: function (elem) {
-            //doWithChildren(elem.parentNode, function (e) {
-            //    if (hasClass(e, 'actionDetail')) {
-            //        e.style.display = 'none';
-            //    }
-            //});
-            var next = findNextSibling(elem, function (elem) {
-                return hasClass(elem, 'actionDetail');
-            });
-            next.style.display = next.style.display === 'block' ? 'none' : 'block';
+            if (elem) {
+                //doWithChildren(elem.parentNode, function (e) {
+                //    if (hasClass(e, 'actionDetail')) {
+                //        e.style.display = 'none';
+                //    }
+                //});
+                var next = nextSiblingWithClass(elem, 'actionDetail');
+                next.style.display = next.style.display === 'block' ? 'none' : 'block';
+            }
         },
-        tryOut: function (button, type, baseUri, path, securitySchemes) {
+        tryOut: function (button, type, path, securitySchemes) {
             showLoader(true);
             showResponse();
-            sendRequest(createRequest(baseUri + path, securitySchemes), handleResponse);
+            sendRequest(createRequest(rd.baseUri + path, securitySchemes), handleResponse);
 
             function createRequest(uri, securitySchemes) {
                 var i, prop,
@@ -92,11 +140,11 @@ var rd = (function () {
             function setRequestValue(req, elem) {
                 var r;
                 if (elem.value) {
-                    if (r = rest(elem.name, 'query_')) {
+                    if (r = rest(elem.name, 'q_')) {
                         req.query += encodeURIComponent(r) + '=' + encodeURIComponent(elem.value) + '&';
-                    } else if (r = rest(elem.name, 'header_')) {
+                    } else if (r = rest(elem.name, 'h_')) {
                         req.header[r] = elem.value;
-                    } else if (r = rest(elem.name, 'uri_')) {
+                    } else if (r = rest(elem.name, 'u_')) {
                         req.uri = req.uri.replace('{' + r + '}', elem.value);
                     } else if (hasClass(elem, 'request') && hasClass(elem, 'contentType')) {
                         req.header['Content-Type'] = elem.value;
@@ -107,9 +155,7 @@ var rd = (function () {
             }
 
             function showLoader(show) {
-                var loader = findNextSibling(button, function (e) {
-                    return hasClass(e, 'loader');
-                });
+                var loader = nextSiblingWithClass(button, 'loader');
                 loader.style.display = show ? 'inline' : 'none';
             }
 
@@ -118,14 +164,12 @@ var rd = (function () {
             }
 
             function showResponse() {
-                var response = findNextSibling(button, function (e) {
-                    return hasClass(e, 'response');
-                });
+                var response = nextSiblingWithClass(button, 'response');
                 response.style.display = 'block';
             }
 
             function sendRequest(r, handler) {
-                var h, url = normalize(interpretUri(r.uri) + '?' + r.query),
+                var h, url = (r.uri + '?' + r.query).replace(/[?&]$/, ''),
                     req = new XMLHttpRequest();
                 req.onreadystatechange = function () {
                     if (req.readyState === 4) {
@@ -147,58 +191,75 @@ var rd = (function () {
                 }
             }
 
-            function normalize(path) {
-                var len;
-                path = path.replace(/\/\.\//g, '/').replace(/[?&]$/, '');
-                do {
-                    len = path.length;
-                    path = path.replace(/\/[^\/]+\/\.\.\//, '/');
-                } while (path.length != len);
-                return path;
-            }
-
-            function interpretUri(uri) {
-                var href = window.location.href,
-                    hostPos = href.indexOf('://'),
-                    pathPos = href.indexOf('/', hostPos + 3),
-                    endPathPos = href.indexOf('/resource/'),
-                    host = href.substring(hostPos + 3, pathPos),
-                    path = href.substring(pathPos + 1, endPathPos);
-                return uri.replace('$host', host).replace('$path', path + '/../..');
-            }
-
             function handleResponse(url, req) {
-                doWithChildren(
-                    findNextSibling(button, function (e) {
-                        return hasClass(e, 'response');
-                    }),
-                    function (elem) {
-                        switch (elem.getAttribute && elem.getAttribute('name')) {
-                        case 'requestUrl':
-                            elem.firstChild.nodeValue = url;
-                            break;
-                        case 'requestHeaders':
-                            var h, s = '';
-                            for (h in req.requestHeaders) {
-                                s += h + ': ' + req.requestHeaders[h] + '\n';
+                var response = nextSiblingWithClass(button, 'response'),
+                    i, reqHeaderStr = '';
+                for (i in req.requestHeaders) {
+                    reqHeaderStr += i + ': ' + req.requestHeaders[i] + '\n';
+                }
+                response.querySelector('[name=requestUrl]').firstChild.nodeValue = url;
+                response.querySelector('[name=requestHeaders]').firstChild.nodeValue = reqHeaderStr;
+                var resBody = response.querySelector('[name=responseBody]');
+                if (isJson(req.getResponseHeader('Content-Type'))) {
+                    resBody.innerHTML = PR.prettyPrintOne(js_beautify(req.responseText));
+                    linkify(resBody);
+                } else {
+                    resBody.firstChild.nodeValue = req.responseText;
+                }
+                response.querySelector('[name=responseCode]').firstChild.nodeValue = req.status + ' ' + req.statusText;
+                response.querySelector('[name=responseHeaders]').firstChild.nodeValue = req.getAllResponseHeaders();
+            }
+
+            function linkify(resBody) {
+                var i, p, a,url,known, strings = resBody.querySelectorAll('.str');
+                for (i = 0; i < strings.length; i++) {
+                    var content = strings[i].firstChild.nodeValue;
+                    if (content.substring(0, 5) === '"http') {
+                        url = content.substring(1, content.length - 1);
+                        known = knownLink(url);
+                        if (known) {
+                            url = document.location.protocol + '//' + document.location.host + document.location.pathname + '/' + rd.relPath + known.url + '.html?';
+                            for (p in known.query) {
+                                url += 'q_' + encodeURIComponent(p) + '=' + encodeURIComponent(known.query[p]) + '&';
                             }
-                            elem.firstChild.nodeValue = s;
-                            break;
-                        case 'responseBody':
-                            if (isJson(req.getResponseHeader('Content-Type'))) {
-                                elem.innerHTML = PR.prettyPrintOne(js_beautify(req.responseText));
-                            } else {
-                                elem.firstChild.nodeValue = req.responseText;
+                            for (p in known.vars) {
+                                url += 'u_' + encodeURIComponent(p) + '=' + encodeURIComponent(known.vars[p]) + '&';
                             }
-                            break;
-                        case 'responseCode':
-                            elem.firstChild.nodeValue = req.status + ' ' + req.statusText;
-                            break;
-                        case 'responseHeaders':
-                            elem.firstChild.nodeValue = req.getAllResponseHeaders();
-                            break;
+                            url += 'method=get&run';
                         }
-                    });
+                        a = document.createElement('a');
+                        a.setAttribute('href', url);
+                        a.setAttribute('target', '_blank');
+                        wrap(strings[i], a);
+                        strings[i].className = 'link';
+                    }
+                }
+            }
+
+            function knownLink(url) {
+                var i, j, patternParts, res, match, parts, qPos;
+                if (url.substring(0, rd.baseUri.length) === rd.baseUri) {
+                    url = url.substring(rd.baseUri.length);
+                    qPos = url.indexOf('?');
+                    parts = (qPos < 0 ? url : url.substring(0, qPos)).split('/');
+                    for (i = 0; i < rd.urls.length; i++) {
+                        patternParts = rd.urls[i].split('/');
+                        if (patternParts.length === parts.length) {
+                            res = {url: rd.urls[i], vars: {}, query: parseQuery(url.substring(qPos))};
+                            match = true;
+                            for (j = 0; j < patternParts.length; j++) {
+                                if (patternParts[j].charAt(0) === '{') {
+                                    res.vars[patternParts[j].substring(1, patternParts[j].length - 1)] = parts[j];
+                                } else if (patternParts[j] !== parts[j]) {
+                                    match = false;
+                                }
+                            }
+                            if (match) {
+                                return res;
+                            }
+                        }
+                    }
+                }
             }
 
             function isJson(mimeType) {
@@ -247,42 +308,38 @@ var rd = (function () {
             }
         },
         initGlobalSecData: function (script) {
-            var data = JSON.parse(sessionStorage.getItem('creds'));
-            doWithChildren(findParent(script, 'div'), function (elem) {
-                if (elem.name === 'logout') {
-                    elem.style.display = data ? 'inline' : 'none';
-                }
-            });
+            var data = JSON.parse(sessionStorage.getItem('creds')),
+                logout = findParent(script, 'div').querySelector('[name=logout]');
+            logout.style.display = data ? 'inline' : 'none';
         },
         showModel: function (span, model) {
-            span.className = 'active';
-            doWithChildren(span.parentNode, function (e) {
-                if (e.nodeName === 'SPAN' && e !== span) {
-                    e.className = 'inactive';
-                }
-                if (hasClass(e, 'model')) {
-                    e.style.display = model ? 'block' : 'none';
-                }
-                if (hasClass(e, 'example')) {
-                    e.style.display = model ? 'none' : 'block';
-                }
-            });
+            span.parentNode.querySelector('.modelTitle').className = (model ? 'active' : 'inactive') + ' modelTitle';
+            span.parentNode.querySelector('.exampleTitle').className = (model ? 'inactive' : 'active') + ' exampleTitle';
+            span.parentNode.querySelector('.model').style.display = model ? 'block' : 'none';
+            span.parentNode.querySelector('.example').style.display = model ? 'none' : 'block';
         },
         useExample: function (div) {
-            var input, code, rawExample = findNextSibling(div.firstChild, function (e) {
-                return hasClass(e, 'rawExample');
-            });
+            var input, code, rawExample = div.querySelector('.rawExample');
             code = (rawExample ? rawExample : div).firstChild.nodeValue;
-            doWithChildren(findParent(div, 'tr'), function (e) {
-                if (e.nodeName === 'TD') {
-                    input = findNextSibling(e.firstChild, function (e) {
-                        return e.nodeName === 'TEXTAREA' || e.nodeName === 'INPUT';
-                    });
-                    if (input) {
-                        input.value = code;
+            findParent(div, 'tr').querySelectorAll('td textarea,td input')[0].value = code;
+        },
+        applyQuery: function () {
+            var inputs, i, q, query = parseQuery(document.location.search);
+            for (q in query) {
+                if (q.charAt(1) === '_') {
+                    inputs = document.querySelectorAll('input[name=' + q + ']');
+                    for (i = 0; i < inputs.length; i++) {
+                        inputs[i].value = query[q];
                     }
                 }
-            });
+            }
+            if (query['method']) {
+                var method = query['method'].toUpperCase();
+                rd.showActionDetail(document.querySelector('.actionHeader.bg_' + method));
+                if (query['run']) {
+                    document.querySelector('.try.' + method).click();
+                }
+            }
         }
     };
 }());
