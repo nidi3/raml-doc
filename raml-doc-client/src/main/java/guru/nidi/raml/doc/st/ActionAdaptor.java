79a472d8c8749519fc8049ff8b0c0fbe472d0126
Stefan Niederhauser <ghuder5@gmx.ch>
2015-09-04 17:18:35 +0200

    cosmetics

--- raml-doc-client/src/main/java/guru/nidi/raml/doc/st/ActionAdaptor.java
+++ raml-doc-client/src/main/java/guru/nidi/raml/doc/st/ActionAdaptor.java
@@ -17,14 +17,10 @@ package guru.nidi.raml.doc.st;
 
 import org.raml.model.Action;
 import org.raml.model.Raml;
-import org.raml.model.SecurityReference;
 import org.stringtemplate.v4.Interpreter;
-import org.stringtemplate.v4.ModelAdaptor;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.misc.ObjectModelAdaptor;
 import org.stringtemplate.v4.misc.STNoSuchPropertyException;
-
-import java.util.List;
 
 /**
  *
