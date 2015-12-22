4468d120aa769f97b99a99772f48d6609e23e50c
Stefan Niederhauser <ghuder5@gmx.ch>
2015-12-22 16:06:06 +0100

    update versions

--- raml-doc-client/src/main/java/guru/nidi/raml/doc/Version.java
+++ raml-doc-client/src/main/java/guru/nidi/raml/doc/Version.java
@@ -19,5 +19,5 @@ package guru.nidi.raml.doc;
  *
  */
 public class Version {
-    public static final String VERSION = "0.0.14-SNAPSHOT";
+    public static final String VERSION = "0.0.15-SNAPSHOT";
 }
